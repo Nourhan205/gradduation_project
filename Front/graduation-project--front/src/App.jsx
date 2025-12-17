feef415f4f2f32c104b594279877ade3118ef8d1
haneen562 <114026530+haneen562@users.noreply.github.com>
2025-12-17 23:39:16 +0200

    Add New Files

--- Front/graduation-project--front/src/App.jsx
+++ Front/graduation-project--front/src/App.jsx
@@ -4,27 +4,33 @@ import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import ForgetPassword from './pages/ForgetPassword';
 import ResetPassword from './pages/ResetPassword';
-import VerificationCode from './pages/VerificationCode'
 import Roadmap from './pages/Roadmap';
+import Chatbot from './pages/Chatbot';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
-      <Routes>
-       
-        <Route path="/" element={<Navigate to="/login" />} />
 
+      <Routes>
+        {/* Landing/Home Page */}
+        <Route path="/" element={<Home />} />
+
+        {/* Auth Pages */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/forgetPassword" element={<ForgetPassword />} />
         <Route path="/reset-password" element={<ResetPassword />} />
+
+        {/* Main Pages */}
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/roadmap" element={<Roadmap />} />
-        <Route path="/verification" element={<VerificationCode />} />
+        <Route path="/chatbot" element={<Chatbot />} />
 
-
-
+        {/* Redirect unknown routes */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
+
   );
 }
 
